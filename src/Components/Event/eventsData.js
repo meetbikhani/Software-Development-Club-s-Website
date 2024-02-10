f79db06121d4b47c7ae97f16f02e7aa5d8c4617b
meetbikhani <bikhanimeet@gmail.com>
2024-02-10 18:10:41 +0530

    event updated

--- src/Components/Event/eventsData.js
+++ src/Components/Event/eventsData.js
@@ -4,17 +4,17 @@ import imageEve2 from '../img/event2.jpg';
 const events = [
   {
     id: 1,
-    title: 'WEB-VIBE',
-    image: event1Img,
-    date: '(Completed)',
-    description:'1 day workshop on Web Development by Sheryians Coding School followed by a Front-End Hackathon',
-  },
-  {
-    id: 1,
     title: 'Ai-CON',
     image: imageEve2,
     date: '(Upcoming)',
     description:`Ai-CON is a groundbreaking event hosted by VIT Bhopal's SDC Club and Google Crowdsource Community. This initiative aims to bring together leading experts from Google India, for insightful speaker sessions on Generative AI. Additionally, we plan to have an auction event with interactive games and competitions through the Google Crowdsource app, participants can earn in-app points to bid on merchandise and more.`,
+  },
+  {
+    id: 1,
+    title: 'WEB-VIBE',
+    image: event1Img,
+    date: '(Completed)',
+    description:'1 day workshop on Web Development by Sheryians Coding School followed by a Front-End Hackathon',
   }
 ];
 
